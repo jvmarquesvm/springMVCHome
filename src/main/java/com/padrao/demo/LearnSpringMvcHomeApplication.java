9c332ea13eab32435d37c55e9f50c742783ec4b7
jvmarquesvm <joaovictor2@hotmail.com>
2019-03-15 00:48:21 -0300

    #1 - Commit Inicial - SpringBoot

---
+++ src/main/java/com/padrao/demo/LearnSpringMvcHomeApplication.java
@@ -0,0 +1,13 @@
+package com.padrao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringMvcHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringMvcHomeApplication.class, args);
+	}
+
+}
